6e1cfcb51873313cf4825cdfd97820c4cc92e87d
M.Rasouli <mehr.rasouli.1402@gmail.com>
2024-04-23 19:10:47 +0330

    data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Data-Binding';
+  userName:string="";
+  password:string="";
+  retypePassword:string="";
+  isPasswordMatch:boolean=false;
+  isLessChar:boolean=false;
+  email:string="";
+  checkEmail:boolean=false;
+  onUsernameInputChanged(e:Event){
+    this.userName=(<HTMLInputElement>e.target).value;
+  }
+  OnPasswordInputChanged(e:Event){
+    this.password=(e.target as HTMLInputElement).value;
+    //this.isLessChar=(this.password.length>=6)?true:false;
+    if(this.password.length>=6){
+      this.isLessChar=true;
+    }
+    else{
+      this.isLessChar=false;
+    }
+  }
+
+  onRetypePasswordChanged(e:Event){
+    this.retypePassword=(e.target as HTMLInputElement).value;
+    //this.isPasswordMatch=(this.password===this.retypePassword)?true:false;
+    if(this.password===this.retypePassword){
+      this.isPasswordMatch=true;
+    }
+    else{
+      this.isPasswordMatch=false;
+    }
+  }
+  onEmailInputChanged(e:Event){
+    this.email=(e.target as HTMLInputElement).value;
+    if(this.email.includes('@')){
+      this.checkEmail=true;
+    }
+    else{
+      this.checkEmail=false;
+    }
+    //this.checkEmail=(this.email.includes('@'))?true:false;
+    
+
+  }
 }
